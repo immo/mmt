a402500b3cc3e46712054c294ffbf8c0df0a15ac
Immanuel Albrecht <github@genetic-superior.de>
2011-01-09 18:03:39 +0100

    ...

--- netbeans/mmtApplet/src/mmt/zeitgerueste/chronologischeAbbildung.java
+++ netbeans/mmtApplet/src/mmt/zeitgerueste/chronologischeAbbildung.java
@@ -2,8 +2,8 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package mmt.zeitgerueste;
+
 import java.util.*;
 import java.io.*;
 
@@ -12,11 +12,30 @@ import java.io.*;
  * @author immanuel
  */
 public class chronologischeAbbildung {
+
     public zeitgeruest source;
     public zeitgeruest target;
     public Map<Integer, Integer> map;
 
     public boolean isNeighborCompatible() {
-        return false;
+        Iterator<int[]> it = source.X.getNeighbors().iterator();
+        while (it.hasNext()) {
+            int[] pair = it.next();
+            int s = pair[0];
+            int t = pair[1];
+
+            if (map.containsKey(s) && map.containsKey(t)) {
+                if (map.get(s) != map.get(t)) {
+                    if (!target.X.isLess(map.get(s), map.get(t))) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void main(String args[])
+            throws java.io.IOException, java.io.FileNotFoundException {
     }
 }
