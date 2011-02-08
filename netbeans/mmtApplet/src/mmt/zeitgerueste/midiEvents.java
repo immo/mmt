211c28b3f97ed453b2ca262b7457bfaf1b9f7049
Immanuel Albrecht <github@genetic-superior.de>
2011-02-08 13:32:06 +0100

    midiEvents class

---
+++ netbeans/mmtApplet/src/mmt/zeitgerueste/midiEvents.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mmt.zeitgerueste;
+import java.io.*;
+import java.util.*;
+
+/**
+ *
+ * @author immanuel
+ */
+public class midiEvents {
+
+}
